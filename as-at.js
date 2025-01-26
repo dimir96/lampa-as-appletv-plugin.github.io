907a00b0396ebe99194c7d4f7427197fdb7b084f
dimir96 <dimir996@gmail.com>
2025-01-26 22:40:09 +0100

    TEST VISUAL

--- as-at.js
+++ as-at.js
@@ -1,10 +1,17 @@
+//test words 
+
+
 (function () {
 	'use strict';
 
-	//Не нужно переписывать весь этот код, не делайте так!
-	//for Lampa.
-	window.lampa_settings.torrents_use = true;
-	window.lampa_settings.demo = false;
-	window.lampa_settings.read_only = false;
+
+    const canvas = document.createElement('canvas');
+    canvas.width = 200;
+    canvas.height = 200;
+    document.body.appendChild(canvas);
+    
+    const ctx = canvas.getContext('2d');
+    ctx.fillStyle = 'blue';
+    ctx.fillRect(0, 0, 200, 200);
 
 })();
