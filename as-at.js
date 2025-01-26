c4bae660bd8e3ce10b56c43de0483c87df5fb0ed
dimir96 <dimir996@gmail.com>
2025-01-26 22:38:42 +0100

    TEST CONNECTION

--- as-at.js
+++ as-at.js
@@ -1,10 +1,10 @@
-//test words 
+(function () {
+	'use strict';
 
-const canvas = document.createElement('canvas');
-canvas.width = 200;
-canvas.height = 200;
-document.body.appendChild(canvas);
+	//Не нужно переписывать весь этот код, не делайте так!
+	//for Lampa.
+	window.lampa_settings.torrents_use = true;
+	window.lampa_settings.demo = false;
+	window.lampa_settings.read_only = false;
 
-const ctx = canvas.getContext('2d');
-ctx.fillStyle = 'blue';
-ctx.fillRect(0, 0, 200, 200);
+})();
