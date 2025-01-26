2f8d7f1d33931ed67696f2f522c30ff10476b3bd
dimir96 <dimir996@gmail.com>
2025-01-26 22:33:06 +0100

    visual test

--- as-at.js
+++ as-at.js
@@ -1,1 +1,10 @@
 //test words 
+
+const canvas = document.createElement('canvas');
+canvas.width = 200;
+canvas.height = 200;
+document.body.appendChild(canvas);
+
+const ctx = canvas.getContext('2d');
+ctx.fillStyle = 'blue';
+ctx.fillRect(0, 0, 200, 200);
