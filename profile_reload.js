f789a855ae3489d3572e77b2d55735b94dc5377b
dimir96 <58175832+dimir96@users.noreply.github.com>
2026-04-27 19:28:25 +0200

    Refactor profile reload logic in profile_reload.js

--- profile_reload.js
+++ profile_reload.js
@@ -1,25 +1,9 @@
 (function () {
-
-    var current = null;
-
-    function getId(val) {
-        try {
-            var obj = typeof val === 'string' ? JSON.parse(val) : val;
-            var p = obj.profile;
-            return String(typeof p === 'object' ? p.id : p);
-        } catch (e) { return null; }
-    }
-
-    current = getId(Lampa.Storage.get('account', '{}'));
-
-    var _orig = Lampa.Storage.set.bind(Lampa.Storage);
+    var current = Lampa.Storage.get('account', {}).profile.id;
+    var orig = Lampa.Storage.set.bind(Lampa.Storage);
     Lampa.Storage.set = function (key, val) {
-        if (key === 'account') {
-            var id = getId(val);
-            if (id && current && id !== current) { window.location.reload(); }
-            else { current = id; }
-        }
-        return _orig(key, val);
+        if (key === 'account' && val.profile.id !== current) window.location.reload();
+        return orig(key, val);
     };
-
 })();
+ 
